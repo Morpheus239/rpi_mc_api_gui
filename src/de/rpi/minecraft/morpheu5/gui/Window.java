b7759e528f920fd9537ba9b36dd6ce222f1fba42
Morpheu5 <Morpheu5@ubuntu>
2013-03-13 23:56:31 +0100

    Extended GUI

--- src/de/rpi/minecraft/morpheu5/gui/Window.java
+++ src/de/rpi/minecraft/morpheu5/gui/Window.java
@@ -27,11 +27,13 @@ import de.rpi.minecraft.morpheu5.controller.Command;
 import de.rpi.minecraft.morpheu5.controller.Controller;
 import de.rpi.minecraft.morpheu5.interfaces.IGuiConsole;
 import de.rpi.minecraft.morpheu5.interfaces.IWindowControl;
+import de.rpi.minecraft.morpheu5.log.rpi_logger;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.logging.Level;
 
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
@@ -43,6 +45,9 @@ public class Window extends JFrame {
 
 	private IWindowControl control;
 	private Command currentCommand = Command.NONE;
+	private JTextField textFieldX;
+	private JTextField textFieldY;
+	private JTextField textFieldZ;
 
 	public Window() {
 		super("RPi MC API GUI");
@@ -60,7 +65,7 @@ public class Window extends JFrame {
 				case 1:
 					currentCommand = Command.WORLD_GETBLOCK;
 					break;
-					
+
 				case 2:
 					currentCommand = Command.PLAYER_GETPOS;
 					break;
@@ -124,7 +129,8 @@ public class Window extends JFrame {
 
 		JTextPane txtDescription = new JTextPane();
 		txtDescription.setEditable(false);
-		txtDescription.setText("Posts a Chat Message on the Minecraft Server");
+		txtDescription
+				.setText("\nPosts a Chat Message on the Minecraft Server");
 		panel_info_description.add(txtDescription, BorderLayout.CENTER);
 
 		JPanel panel_info_input = new JPanel();
@@ -146,8 +152,7 @@ public class Window extends JFrame {
 		panel_data_input.add(label_data_message);
 
 		final JTextField textField_data_input = new JTextField();
-		
-		
+
 		textField_data_input.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_data_input.add(textField_data_input);
 		textField_data_input.setColumns(10);
@@ -166,8 +171,7 @@ public class Window extends JFrame {
 
 		JButton btnSend = new JButton("Send");
 		btnSend.setMnemonic('s');
-		
-		
+
 		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -188,34 +192,7 @@ public class Window extends JFrame {
 
 		final JList list_commands = new JList();
 		list_commands.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list_commands.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent arg0) {
-				if (arg0.getValueIsAdjusting()) {
-					switch (list_commands.getSelectedIndex()) {
-					case 0:
-						currentCommand = Command.WORLD_GETBLOCK;
-						break;
 
-					case 1:
-						currentCommand = Command.WORLD_GETBLOCKWITHDATA;
-						break;
-
-					case 2:
-						currentCommand = Command.WORLD_SETBLOCK;
-						break;
-
-					case 9:
-						currentCommand = Command.PLAYER_GETPOS;
-						break;
-
-					default:
-						break;
-					}
-
-				}
-
-			}
-		});
 		list_commands.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(
 				0, 0, 0)));
 		list_commands.setPreferredSize(new Dimension(200, 0));
@@ -223,7 +200,7 @@ public class Window extends JFrame {
 		list_commands.setModel(new AbstractListModel() {
 			String[] values = new String[] { "getBlock", "getBlockWithData",
 					"setBlock", "setBlocks", "getHeight", "getPlayerIds",
-					"setting", "checkpoint.save", "checkpoint.restore", "DUMMY" };
+					"setting", "checkpoint.save", "checkpoint.restore" };
 
 			public int getSize() {
 				return values.length;
@@ -245,16 +222,17 @@ public class Window extends JFrame {
 		panel_info.add(panel_info_description);
 		panel_info_description.setLayout(new BorderLayout(0, 0));
 
-		JLabel lblHeadline = new JLabel("COMMAND");
+		final JLabel lblHeadline = new JLabel("setBlocks");
 		lblHeadline.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0,
 				0, 0)));
 		lblHeadline.setBackground(Color.WHITE);
-		lblHeadline.setFont(new Font("Dialog", Font.BOLD, 30));
+		lblHeadline.setFont(new Font("Dialog", Font.BOLD, 25));
 		panel_info_description.add(lblHeadline, BorderLayout.NORTH);
 
-		JTextPane txtDescription = new JTextPane();
+		final JTextPane txtDescription = new JTextPane();
 		txtDescription.setEditable(false);
-		txtDescription.setText("DESC");
+		txtDescription
+				.setText("Sets all blocks in a cuboid defined by two points to a speficied type. The blockData parameter is optional.\n\nx1, y1, z1: coordinates of the first selection point relative to spawn\nx2, y2, z2: coordinates of the second selection point relative to spawn\n\nblockTypeId: ID of the block\nblockData: metadata of the block ");
 		panel_info_description.add(txtDescription, BorderLayout.CENTER);
 
 		JPanel panel_info_input = new JPanel();
@@ -269,16 +247,40 @@ public class Window extends JFrame {
 
 		JPanel panel_data_input = new JPanel();
 		panel_info_input.add(panel_data_input, BorderLayout.CENTER);
-		panel_data_input.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		panel_data_input.setLayout(new GridLayout(0, 1, 0, 0));
 
-		JLabel label_data_message = new JLabel("Message:");
-		label_data_message.setFont(new Font("Dialog", Font.BOLD, 20));
-		panel_data_input.add(label_data_message);
+		JPanel panel_x = new JPanel();
+		panel_data_input.add(panel_x);
 
-		final JTextField textField_data_input = new JTextField();
-		textField_data_input.setHorizontalAlignment(SwingConstants.LEFT);
-		panel_data_input.add(textField_data_input);
-		textField_data_input.setColumns(10);
+		JLabel labelX = new JLabel("X");
+		panel_x.add(labelX);
+
+		textFieldX = new JTextField();
+		textFieldX.setColumns(10);
+		panel_x.add(textFieldX);
+
+		JPanel panel_y = new JPanel();
+		panel_data_input.add(panel_y);
+
+		JLabel labelY = new JLabel("Y");
+		panel_y.add(labelY);
+
+		textFieldY = new JTextField();
+		textFieldY.setColumns(10);
+		panel_y.add(textFieldY);
+
+		JPanel panel_z = new JPanel();
+		panel_data_input.add(panel_z);
+
+		JLabel labelZ = new JLabel("Z");
+		panel_z.add(labelZ);
+
+		textFieldZ = new JTextField();
+		textFieldZ.setColumns(10);
+		panel_z.add(textFieldZ);
+
+		JLabel label = new JLabel("");
+		panel_data_input.add(label);
 
 		JPanel panel_data_action = new JPanel();
 		panel_info_input.add(panel_data_action, BorderLayout.SOUTH);
@@ -287,10 +289,80 @@ public class Window extends JFrame {
 		JButton btnClear = new JButton("Clear");
 		btnClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				textField_data_input.setText("");
 			}
 		});
 		panel_data_action.add(btnClear);
+
+		list_commands.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+
+				if (arg0.getValueIsAdjusting()) {
+
+					// Select Command by JListselection
+					switch (list_commands.getSelectedIndex()) {
+					case 0:
+						currentCommand = Command.WORLD_GETBLOCK;
+						lblHeadline.setText("getBlock");
+						txtDescription
+								.setText("\nGets the ID of a block in the world.\n\nx, y, z: "
+										+ "coordinates of the block relative to spawn\n\nReturns: an integer of type blockTypeId, "
+										+ "corresponding to a block type in Minecraft Pi. ");
+
+						break;
+
+					case 1:
+						currentCommand = Command.WORLD_GETBLOCKWITHDATA;
+						lblHeadline.setText("getBlockWithData");
+						txtDescription
+								.setText("\nGets the ID and Optional Parameter for a block in the world.\n\nx, y, z: coordinates of the block relative to spawn\n\nReturns: a block object corresponding toa block type in Minecraft Pi. ");
+
+						break;
+
+					case 2:
+						currentCommand = Command.WORLD_SETBLOCK;
+						lblHeadline.setText("setBlock");
+						txtDescription
+								.setText("\nSets the ID of a block in the world. The data parameter is optional.\n\nx, y, z: coordinates of the block relative to spawn\n\nblockTypeId: ID of the block\n\nblockData: metadata of the block ");
+
+						break;
+
+					case 3:
+						currentCommand = Command.WORLD_SETBLOCKS;
+						lblHeadline.setText("setBlocks");
+						txtDescription
+						.setText("Sets all blocks in a cuboid defined by two points to a speficied type. The blockData parameter is optional.\n\nx1, y1, z1: coordinates of the first selection point relative to spawn\nx2, y2, z2: coordinates of the second selection point relative to spawn\n\nblockTypeId: ID of the block\nblockData: metadata of the block ");
+						break;
+
+					case 4:
+						currentCommand = Command.WORLD_GETHEIGHT;
+						break;
+
+					case 5:
+						currentCommand = Command.WORLD_GETPLAYERIDS;
+						break;
+
+					case 6:
+						currentCommand = Command.WORLD_SETTING;
+						break;
+
+					case 7:
+						currentCommand = Command.WORLD_CHECKPOINT_SAVE;
+						break;
+
+					case 8:
+						currentCommand = Command.WORLD_CHECKPOINT_RESTORE;
+						break;
+
+					default:
+						rpi_logger.LOGGER.log(Level.FINE,
+								"JList not right checked");
+						break;
+					}
+
+				}
+
+			}
+		});
 
 		JButton btnSend = new JButton("Send");
 		btnSend.addActionListener(new ActionListener() {
@@ -333,8 +405,7 @@ public class Window extends JFrame {
 					switch (list_commands.getSelectedIndex()) {
 					case 0:
 						currentCommand = Command.PLAYER_GETPOS;
-						
-						
+
 						break;
 
 					default:
